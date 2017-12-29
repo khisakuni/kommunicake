1a5a18593e2f2287c57c4cf25714edf4913674fc
Kohei Hisakuni <kohei@philosophie.is>
2017-12-29 15:00:22 -0600

    Fix

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,14 @@
 package main
 
 import "github.com/khisakuni/kommunicake/app"
+import "github.com/khisakuni/kommunicake/db"
+import "github.com/khisakuni/kommunicake/router"
 
 func main() {
-	a, err := app.NewApp()
+	d, err := db.NewDB()
+	r := router.NewRouter()
+
+	a := app.NewApp(d, r)
 	if err != nil {
 		panic(err)
 	}
